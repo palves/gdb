9c3dbc95abe2fd005f771a1b0f96f9d88151f6ae
Pedro Alves <pedro@codesourcery.com>
2011-12-17 02:23:01 +0000

    Parallel steps, wait for all threads to reach their goal
    Something I mentioned in previous patches.  This makes it so that with e.g.,
    
    	(gdb) step -p t1,t2
    
    	you only get the prompt back when both t1 and t2 finished
    	stepping.
    
    2011-12-16  Pedro Alves  <pedro@codesourcery.com>
    
    	* breakpoint.c (until_break_command): Adjust.
    	* gdbthread.h (struct itset): Forward declare.
    	(struct thread_info) <apply_set, waiting_for_apply_set_siblings>:
    	New fields.
    	* infcmd.c (apply_execution_command): New parameter
    	`want_parallel'.  Handle it.
    	(continue_command, step_1, jump_command, signal_command)
    	(until_next_command, finish_command): Adjust.
    	* infrun.c (clear_proceed_status_thread): Clear the thread's apply
    	set and waiting_for_apply_set_siblings flag.
    	(all_in_apply_set_done_p): New.
    	(handle_inferior_event): Check if there are other thread's in the
    	even thread's apply set.  If so, wait for them to finish before
    	considering the execution command done with.

--- gdb/infrun.h
+++ gdb/infrun.h
@@ -222,6 +222,7 @@ extern int follow_fork (int prepare_only);
 extern void
   apply_execution_command (struct itset *apply_itset,
 			   struct itset *run_free_itset,
+			   int want_parallel,
 			   aec_callback_func callback, void *callback_data);
 
 #endif /* INFRUN_H */
