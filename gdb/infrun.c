5c03eeec0235aeddd119ffc37e95bb6840d55620
Pedro Alves <palves@redhat.com>
2015-05-04 19:29:43 +0100

    fix parallel_step_wait_all

--- gdb/infrun.c
+++ gdb/infrun.c
@@ -2879,7 +2879,7 @@ clear_proceed_status (int step)
 				     execution_direction))
     target_record_stop_replaying ();
 
-  if (!non_stop)
+  if (!target_is_non_stop_p ())
     {
       struct thread_info *tp;
 
@@ -2904,7 +2904,7 @@ clear_proceed_status (int step)
     {
       struct inferior *inferior;
 
-      if (non_stop)
+      if (target_is_non_stop_p ())
 	{
 	  /* If in non-stop mode, only delete the per-thread status of
 	     the current thread.  */
@@ -4967,7 +4967,9 @@ all_in_apply_set_done_p (struct execution_control_state *ecs)
       struct thread_info *thr;
 
       if (debug_infrun)
-	fprintf_unfiltered (gdb_stdlog, "infrun: thread has an apply-set\n");
+	fprintf_unfiltered (gdb_stdlog, "infrun: thread %s [%s] has an apply-set\n",
+			    print_thread_id (ecs->event_thread),
+			    target_pid_to_str (ecs->event_thread->ptid));
 
       update_thread_list ();
 
@@ -4980,7 +4982,8 @@ all_in_apply_set_done_p (struct execution_control_state *ecs)
 	      {
 		if (debug_infrun)
 		  fprintf_unfiltered (gdb_stdlog,
-				      "infrun: at least thread %s has not reached its goal yet\n",
+				      "infrun: at least thread %s has not "
+				      "reached its goal yet\n",
 				      target_pid_to_str (thr->ptid));
 
 		prepare_to_wait (ecs);
