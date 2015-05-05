d3fadf4977368c254386ef35e6ec424e2f420877
Pedro Alves <palves@redhat.com>
2015-05-06 00:21:24 +0100

    don't call inferior_thread if there's no selected thread

--- gdb/itset.c
+++ gdb/itset.c
@@ -721,7 +721,12 @@ thread_range_get_toi (struct itset_elt *base)
   struct thread_info *thr;
 
   if (range->is_current)
-    return inferior_thread ();
+    {
+      if (ptid_equal (inferior_ptid, null_ptid))
+	return NULL;
+      else
+	return inferior_thread ();
+    }
 
   inf = find_inferior_id (range->inf_num);
 
