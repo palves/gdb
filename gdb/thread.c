18b8e761a98fd51259f9ef3e174e9cb3b678b4c1
Pedro Alves <palves@redhat.com>
2015-05-06 00:14:02 +0100

    revert focus switching on stop or "thread N" commands

--- gdb/thread.c
+++ gdb/thread.c
@@ -1899,7 +1899,7 @@ do_captured_thread_select (struct ui_out *uiout, void *tidstr)
 
   switch_to_thread (tp->ptid);
 
-  itfocus_from_thread_switch ();
+  // itfocus_from_thread_switch ();
 
   annotate_thread_changed ();
 
