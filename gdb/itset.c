8ddc7b63310edc91e25229917d03553194a52d40
Pedro Alves <palves@redhat.com>
2015-05-18 15:42:04 +0100

    make explicit widths actually start working

--- gdb/itset.c
+++ gdb/itset.c
@@ -1004,13 +1004,10 @@ thread_range_clone (struct itset_elt *base)
 				     &thread_range_elt->inf_range,
 				     &range->range);
 
-#if 0
-  /* FIXME */
   if (range->explicit_width != NULL)
     clone->explicit_width
-      = range->explicit_width->clone (range->explicit_width);
+      = itset_reference (range->explicit_width);
   else
-#endif
     clone->explicit_width = NULL;
 
   /* FIXME */
@@ -2958,7 +2955,6 @@ parse_elem_1 (struct itset_parser *self)
 
 	  elt = VEC_index (itset_elt_ptr, current_itset->elements, 0);
 
-#if 0
 	  if (elt->vtable->clone != NULL)
 	    {
 	      struct itset_elt *clone_elt = elt->vtable->clone (elt);
@@ -2971,7 +2967,6 @@ parse_elem_1 (struct itset_parser *self)
 		range_elt->group = group;
 	      return (struct itset_elt *) range_elt;
 	    }
-#endif
 	}
 
       error (_("Current focus is a complex set, and no ID specified."));
