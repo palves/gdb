a94994ee705c445d161bf4534a90591603c38b14
Pedro Alves <palves@redhat.com>
2015-08-13 19:35:36 +0100

    fix async console events

--- gdb/event-top.c
+++ gdb/event-top.c
@@ -1352,6 +1352,7 @@ new_console_command (char *args, int from_tty)
 
   //  interp = interp_create (INTERP_CONSOLE);
   interp = interp_create (INTERP_TUI);
+  interp_add (interp);
   interp_set (interp, 0);
 
   printf_unfiltered ("Hello from new GDB console\n");
