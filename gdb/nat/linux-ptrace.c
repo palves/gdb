6fe10bf4035aeb9286b358898f7426e09750a471
Pedro Alves <palves@redhat.com>
2016-12-03 22:58:12 +0000

    linux-ptrace.c

--- gdb/nat/linux-ptrace.c
+++ gdb/nat/linux-ptrace.c
@@ -25,6 +25,18 @@
 #include <sys/procfs.h>
 #endif
 
+#if defined __i386__ || defined __x86_64__
+# include <sys/reg.h>
+# include <sys/mman.h>
+# include <signal.h>
+
+/* Address of the 'ret' instruction in asm code block below.  */
+EXTERN_C void linux_ptrace_test_ret_to_nx_instr (void);
+
+#endif /* __i386__ || defined __x86_64__ */
+
+namespace gdb {
+
 /* Stores the ptrace options supported by the running kernel.
    A value of -1 means we did not check for features yet.  A value
    of 0 means there are no supported features.  */
@@ -67,17 +79,6 @@ linux_ptrace_attach_fail_reason_string (ptid_t ptid, int err)
   else
     return string_printf ("%s (%d)", safe_strerror (err), err);
 }
-
-#if defined __i386__ || defined __x86_64__
-
-/* Address of the 'ret' instruction in asm code block below.  */
-EXTERN_C void linux_ptrace_test_ret_to_nx_instr (void);
-
-#include <sys/reg.h>
-#include <sys/mman.h>
-#include <signal.h>
-
-#endif /* defined __i386__ || defined __x86_64__ */
 
 /* Kill CHILD.  WHO is used to report warnings.  */
 
@@ -614,3 +615,5 @@ linux_wstatus_maybe_breakpoint (int wstat)
 	      || WSTOPSIG (wstat) == SIGILL
 	      || WSTOPSIG (wstat) == SIGSEGV));
 }
+
+} /* namespace gdb */
