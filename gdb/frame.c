77eae2156d30f2c39a32c6e81dd8595992244df8
Pedro Alves <palves@redhat.com>
2016-12-03 23:54:17 +0000

    frame.c

--- gdb/frame.c
+++ gdb/frame.c
@@ -43,6 +43,8 @@
 #include "hashtab.h"
 #include "valprint.h"
 
+namespace gdb {
+
 /* The sentinel frame terminates the innermost end of the frame chain.
    If unwound, it returns the information needed to construct an
    innermost frame.
@@ -2951,3 +2953,5 @@ When non-zero, frame specific internal debugging is enabled."),
 			     show_frame_debug,
 			     &setdebuglist, &showdebuglist);
 }
+
+} /* namespace gdb */
