06e42c6e6047115469c54208a5abbd3410afc6e8
Pedro Alves <palves@redhat.com>
2017-10-19 19:19:10 +0100

    discard_pending_stop_replies

--- gdb/remote.c
+++ gdb/remote.c
@@ -421,6 +421,7 @@ private:
 
   void remove_new_fork_children (threads_listing_context *context);
   void kill_new_fork_children (int pid, remote_state *rs);
+  void discard_pending_stop_replies (struct inferior *inf);
 
   friend class vcont_builder;
 };
@@ -7017,8 +7018,8 @@ remove_stop_reply_for_inferior (QUEUE (stop_reply_p) *q,
 
 /* Discard all pending stop replies of inferior INF.  */
 
-static void
-discard_pending_stop_replies (struct inferior *inf)
+void
+remote_target::discard_pending_stop_replies (struct inferior *inf)
 {
   struct queue_iter_param param;
   struct stop_reply *reply;
@@ -9833,6 +9834,10 @@ void
 remote_target::mourn_inferior ()
 {
   struct remote_state *rs = get_remote_state ();
+
+  /* We're no longer interested in notification events of an inferior
+     when it exits.  */
+  discard_pending_stop_replies (current_inferior ());
 
   /* In 'target remote' mode with one inferior, we close the connection.  */
   if (!rs->extended && number_of_live_inferiors () <= 1)
@@ -14260,9 +14265,6 @@ _initialize_remote (void)
 
   /* Hook into new objfile notification.  */
   observer_attach_new_objfile (remote_new_objfile);
-  /* We're no longer interested in notification events of an inferior
-     when it exits.  */
-  observer_attach_inferior_exit (discard_pending_stop_replies);
 
 #if 0
   init_remote_threadtests ();
