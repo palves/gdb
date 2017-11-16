04f1ff0c4e6c01fb83af90294d9a8a0166443a96
Pedro Alves <palves@redhat.com>
2017-11-16 17:50:11 +0000

    target_terminal::ours_for_output(): received SIGINT
    ~~~
      $ $g -q --args $g -q ./testsuite/outputs/gdb.base/interrupt/interrupt
      Reading symbols from ./gdb...done.
      Setting up the environment for debugging gdb.
      Breakpoint 1 at 0x6a6fdd: file /home/pedro/gdb/mygit/src/gdb/common/errors.c, line 54.
      Breakpoint 2 at 0x4af9ac: file /home/pedro/gdb/mygit/src/gdb/cli/cli-cmds.c, line 197.
      (top-gdb) r
      Starting program: /home/pedro/brno/pedro/gdb/mygit/build/gdb/gdb -data-directory=data-directory -q ./testsuite/outputs/gdb.base/interrupt/interrupt
      [Thread debugging using libthread_db enabled]
      Using host libthread_db library "/lib64/libthread_db.so.1".
      [New Thread 0x7fffed01e700 (LWP 23630)]
      [New Thread 0x7fffec81d700 (LWP 23631)]
      [New Thread 0x7fffec01c700 (LWP 23632)]
      [New Thread 0x7fffeb81b700 (LWP 23633)]
      [New Thread 0x7fffeb01a700 (LWP 23634)]
      [New Thread 0x7fffea819700 (LWP 23635)]
      [New Thread 0x7fffea018700 (LWP 23636)]
      Reading symbols from ./testsuite/outputs/gdb.base/interrupt/interrupt...done.
      (gdb) r
      Starting program: /home/pedro/brno/pedro/gdb/mygit/build/gdb/testsuite/outputs/gdb.base/interrupt/interrupt
      talk to me baby
    
      Thread 1 "gdb" received signal SIGINT, Interrupt.
    						   0x00007ffff551fb7d in poll () at ../sysdeps/unix/syscall-template.S:84
      84      T_PSEUDO (SYSCALL_SYMBOL, SYSCALL_NAME, SYSCALL_NARGS)
      (top-gdb)
    ~~~
                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    Note the missing carriage return above.

--- gdb/infrun.c
+++ gdb/infrun.c
@@ -8182,7 +8182,10 @@ normal_stop (void)
   update_thread_list ();
 
   if (last.kind == TARGET_WAITKIND_STOPPED && stopped_by_random_signal)
-    observer_notify_signal_received (inferior_thread ()->suspend.stop_signal);
+    {
+      target_terminal::ours_for_output ();
+      observer_notify_signal_received (inferior_thread ()->suspend.stop_signal);
+    }
 
   /* As with the notification of thread events, we want to delay
      notifying the user that we've switched thread context until
