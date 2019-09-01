f4b7f0e4db98f1fb4e6c2bf6431408ea35ac5321
Pedro Alves <palves@redhat.com>
2019-09-01 19:35:18 +0100

    Make target_ops::has_execution take an 'inferior *' instead of a ptid_t
    With the multi-target work, each inferior will have its own target
    stack, so to call a target method, we'll need to make sure that the
    inferior in question is the current one, otherwise target->beneath()
    calls will find the target beneath in the wrong inferior.
    
    In some places, it's much more convenient to be able to check whether
    an inferior has execution without having to switch to it in order to
    call target_has_execution on the right inferior/target stack, to avoid
    side effects with switching inferior/thread/program space.
    
    The current target_ops::has_execution method takes a ptid_t as
    parameter, which, in a multi-target world, isn't sufficient to
    identify the target.  This patch prepares to address that, by changing
    the parameter to an inferior pointer instead.  From the inferior,
    we'll be able to query its target stack to tell which target is
    beneath.
    
    Also adds a new inferior::has_execution() method to make callers a bit
    more natural to read.
    
    gdb/ChangeLog:
    yyyy-mm-dd  Pedro Alves  <palves@redhat.com>
    
    	* corelow.c (core_target::has_execution): Change parameter type to
    	inferior pointer.
    	* inferior.c (number_of_live_inferiors): Use
    	inferior::has_execution instead of target_has_execution_1.
    	* inferior.h (inferior::has_execution): New.
    	* linux-thread-db.c (thread_db_target::update_thread_list): Use
    	inferior::has_execution instead of target_has_execution_1.
    	* process-stratum-target.c
    	(process_stratum_target::has_execution): Change parameter type to
    	inferior pointer.  Check the inferior's PID instead of
    	inferior_ptid.
    	* process-stratum-target.h
    	(process_stratum_target::has_execution): Change parameter type to
    	inferior pointer.
    	* record-full.c (record_full_core_target::has_execution): Change
    	parameter type to inferior pointer.
    	* target.c (target_has_execution_1): Change parameter type to
    	inferior pointer.
    	(target_has_execution_current): Adjust.
    	* target.h (target_ops::has_execution): Change parameter type to
    	inferior pointer.
    	(target_has_execution_1): Change parameter type to inferior
    	pointer.  Change return type to bool.
    	* tracefile.h (tracefile_target::has_execution): Change parameter
    	type to inferior pointer.

--- gdb/target.h
+++ gdb/target.h
@@ -682,7 +682,7 @@ struct target_ops
     virtual bool has_memory () { return false; }
     virtual bool has_stack () { return false; }
     virtual bool has_registers () { return false; }
-    virtual bool has_execution (ptid_t) { return false; }
+    virtual bool has_execution (inferior *inf) { return false; }
 
     /* Control thread execution.  */
     virtual thread_control_capabilities get_thread_control_capabilities ()
@@ -1787,9 +1787,10 @@ extern int target_has_registers_1 (void);
    case this will become true after to_create_inferior or
    to_attach.  */
 
-extern int target_has_execution_1 (ptid_t);
+extern bool target_has_execution_1 (inferior *inf);
 
-/* Like target_has_execution_1, but always passes inferior_ptid.  */
+/* Like target_has_execution_1, but always passes
+   current_inferior().  */
 
 extern int target_has_execution_current (void);
 
